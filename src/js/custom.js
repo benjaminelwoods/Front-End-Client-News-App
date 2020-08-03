f35c2157f5c2c1c1866defcbc836d4d4a0e38ff8
Ben Elwood <ben.elwoods@gmail.com>
2020-08-04 11:11:49 +1200

    basic styling

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,9 +1,34 @@
 // custom js file
+var axios = require('axios');
 
 (function(){
 
-	alert('working');
+
+	// import Vue from 'vue/dist/vue.js';
+
+	console.log('hello')
+
+	let app = new Vue ({
+		el: '#app',
+		data: {
+			topHeadlines: false
+		}
+	});
 
 
+	function axiosCall () {
+		alert('working')
+		console.log('working')
+		axios({
+			method: 'get',
+			url: 'http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=0ec38d938d324e70a359a09a2ff04048'
+		})
+		.then(function (response){
+			app.topHeadlines = response
+			console.log(app.topHeadlines)
+		});
+	}
+
+	axiosCall()
 
 })(); // iffe ENDS
