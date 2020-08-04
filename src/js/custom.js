133e856884dac94e6ca328beb1c2bda40432fa10
Ben Elwood <ben.elwoods@gmail.com>
2020-08-05 10:50:30 +1200

    added onclick events to articles to expand the view and read extended text

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,22 +3,46 @@ var axios = require('axios');
 
 (function(){
 
-
-	// import Vue from 'vue/dist/vue.js';
-
-	// console.log('hello')
-
 	let app = new Vue ({
 		el: '#app',
 		data: {
 			topHeadlines: false
+		},
+		methods: {
+			articleFullView: function (num) {
+				const backBtn = document.getElementById('backButton');
+				let newsBlocks = document.getElementsByClassName('newsBlock');
+				backBtn.classList.remove('hideBtn');
+
+				for (var i = 0; i < newsBlocks.length; i++) {
+					newsBlocks[i].classList.add('hideMe');
+				}
+				newsBlocks[num].classList.remove('hideMe');
+				newsBlocks[num].classList.add('fullView');
+				newsBlocks[num].lastChild.lastChild.classList.add('showArticle')
+			},
+			normalView: function () {
+				const backBtn = document.getElementById('backButton');
+				let newsBlocks = document.getElementsByClassName('newsBlock');
+
+				backBtn.classList.add('hideBtn');
+
+				for (var i = 0; i < newsBlocks.length; i++) {
+					newsBlocks[i].classList.remove('hideMe');
+				}
+				let fullViewBlock = document.getElementsByClassName('fullView');
+				fullViewBlock[0].classList.add('hideAnim');
+				fullViewBlock[0].lastChild.lastChild.classList.remove('showArticle')
+				setTimeout(function(){
+					fullViewBlock[0].classList.remove('hideAnim');
+					fullViewBlock[0].classList.remove('fullView');
+				}, 400);
+			}
 		}
 	});
 
 
 	function axiosCall () {
-		// alert('working')
-		console.log('working');
 		axios({
 			method: 'get',
 			url: 'http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=0ec38d938d324e70a359a09a2ff04048'
@@ -30,14 +54,20 @@ var axios = require('axios');
 
 	axiosCall();
 
-	let newsBlocks = document.getElementsByClassName('newsBlock');
+
 
 	// console.log(newsBlocks)
 
-	for (var i = 0; i < newsBlocks.length; i++) {
-		// newsBlocks[i].addEventListener('click', newsFullView)
-		console.log('heeeeeelo');
-	}
+	// for (var i = 0; i < 5; i++) {
+	// 	console.log(i)
+	// }
+
+	// console.log(newsBlocks)
+
+	// for (var i = 0; i < 15; i++) {
+	// 	// newsBlocks[i].addEventListener('click', newsFullView)
+	// 	console.log('heeeeeelo');
+	// }
 
 	// function newsFullView () {
 	// 	console.log('working')
