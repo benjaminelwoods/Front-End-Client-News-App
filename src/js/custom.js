f2f2aa56c2f990fd3abacb0d86665c414323b9d3
benjaminelwoods <ben.elwoods@gmail.com>
2020-08-07 08:40:34 +1200

    auto populated the dom based on the amount of article results

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,67 +3,159 @@ var axios = require('axios');
 
 (function(){
 
+
+	// console.log(test)
+
+	// newBlock.innerHtml =
+	// <img v-bind:src="newsData.data.articles[0].urlToImage">
+	// <div class="textCont">
+	// 	<h2>{{newsData.data.articles[0].title}}</h2>
+	// 	<h3>{{newsData.data.articles[0].author}} <span class="date">{{newsData.data.articles[0].publishedAt}}</span></h3>
+	// 	<p class="article">{{newsData.data.articles[0].description}}</p>
+	// </div>;
+
+	// function createArticleBlock (num) {
+	//
+	// }
+	//
+	// createArticleBlock(0)
+	// initialAxiosCall();
+
+	let booleans = {
+		articleOpen: false,
+		totalResults: false
+	};
+
+	let articleArr = [];
+
 	let app = new Vue ({
 		el: '#app',
 		data: {
 			newsData: false
+			// totalResults: false
+		},
+		created: function () {
+			axiosCall()
+			setTimeout(function (){
+				// createArticleBlock(booleans.totalResults);
+				num = booleans.totalResults
+				// console.log('createart')
+				// console.log(num);
+				let newsCont = document.getElementById('newsContainer');
+				for (var i = 0; i < num; i++) {
+					let newBlock = document.createElement('DIV');
+					let newImg = document.createElement('IMG');
+					let newTextCont = document.createElement('DIV');
+					let newTitle = document.createElement('H2');
+					let newAuthor = document.createElement('H3');
+					let newDesc = document.createElement('P');
+
+					// newBlock.setAttribute('v-on:click', 'articleFullView(' + i + ')');
+					newBlock.setAttribute('id', 'article-' + i);
+					articleArr.push(newBlock)
+					newImg.setAttribute('src', app.newsData.data.articles[i].urlToImage);
+					newBlock.classList.add('newsBlock');
+					newTextCont.classList.add('textCont');
+					newDesc.classList.add('article');
+					newTitle.textContent = app.newsData.data.articles[i].title;
+					newAuthor.textContent = app.newsData.data.articles[i].author;
+					newDesc.textContent = app.newsData.data.articles[i].description;
+					newsCont.appendChild(newBlock);
+					newBlock.appendChild(newImg);
+					newBlock.appendChild(newTextCont);
+					newTextCont.appendChild(newAuthor);
+					newTextCont.appendChild(newTitle);
+					newTextCont.appendChild(newDesc);
+				}
+				console.log(articleArr)
+				addListeners()
+			}, 1000)
 		},
 		methods: {
-			articleFullView: function (num) {
-				const backBtn = document.getElementById('backButton');
-				let newsBlocks = document.getElementsByClassName('newsBlock');
-				backBtn.classList.remove('hideBtn');
-
-				for (var i = 0; i < newsBlocks.length; i++) {
-					newsBlocks[i].classList.add('hideMe');
-				}
-				newsBlocks[num].classList.remove('hideMe');
-				newsBlocks[num].classList.add('fullView');
-				newsBlocks[num].lastChild.lastChild.classList.add('showArticle')
-			},
-			normalView: function () {
-				const backBtn = document.getElementById('backButton');
-				let newsBlocks = document.getElementsByClassName('newsBlock');
-
-				backBtn.classList.add('hideBtn');
-
-				for (var i = 0; i < newsBlocks.length; i++) {
-					newsBlocks[i].classList.remove('hideMe');
-				}
-				let fullViewBlock = document.getElementsByClassName('fullView');
-				fullViewBlock[0].classList.add('hideAnim');
-				fullViewBlock[0].lastChild.lastChild.classList.remove('showArticle')
-				setTimeout(function(){
-					fullViewBlock[0].classList.remove('hideAnim');
-					fullViewBlock[0].classList.remove('fullView');
-				}, 400);
-			},
-			querySearch: function () {
-				let query = document.getElementById('searchInput').value
-				axios({
-					method: 'get',
-					url: 'http://newsapi.org/v2/top-headlines?' + 'q=' + query + '&' + 'apiKey=0ec38d938d324e70a359a09a2ff04048'
-				})
-				.then(function (response){
-					app.newsData = false;
-					app.newsData = response;
-					console.log(response)
-				});
-			}
+			// articleFullView: function (num) {
+			// 	console.log(num)
+			// 	let newsBlocks = document.getElementsByClassName('newsBlock');
+			// 	if (booleans.articleOpen == true) {
+			// 		booleans.articleOpen = false;
+			// 		newsBlocks[num].lastChild.lastChild.classList.remove('showArticle');
+			// 	} else if (booleans.articleOpen == false) {
+			// 		booleans.articleOpen = true;
+			// 		newsBlocks[num].lastChild.lastChild.classList.add('showArticle');
+			// 	}
+			// }
+			// querySearch: function () {
+			// 	let query = document.getElementById('searchInput').value;
+			// 	axios({
+			// 		method: 'get',
+			// 		url: 'http://newsapi.org/v2/top-headlines?' + 'q=' + query + '&' + 'apiKey=0ec38d938d324e70a359a09a2ff04048'
+			// 	})
+			// 	.then(function (response){
+			// 		app.newsData = false;
+			// 		app.newsData = response;
+			// 	});
+			// }
 		}
 	});
 
+function articleFullView (num) {
+	console.log(num)
+	let newsBlocks = document.getElementsByClassName('newsBlock');
+	if (booleans.articleOpen == true) {
+		booleans.articleOpen = false;
+		newsBlocks[num].lastChild.lastChild.classList.remove('showArticle');
+	} else if (booleans.articleOpen == false) {
+		booleans.articleOpen = true;
+		newsBlocks[num].lastChild.lastChild.classList.add('showArticle');
+	}
+}
+
+function addListeners () {
+	for (var i = 0; i < articleArr.length; i++) {
+		let num = articleArr[i].id.replace('article-', '')
+		articleArr[i].addEventListener('click', function() {articleFullView(num)}, false)
+	}
+}
+	// function initialAxiosCall () {
+	// 	axios({
+	// 		method: 'get',
+	// 		url: 'http://newsapi.org/v2/top-headlines?' + 'country=nz&' + 'apiKey=0ec38d938d324e70a359a09a2ff04048'
+	// 	})
+	// 	.then(function (response){
+	// 		console.log(response.data.totalResults)
+	// 		booleans.totalResults = response.data.totalResults
+	// 	});
+	// }
 
 	function axiosCall () {
 		axios({
 			method: 'get',
-			url: 'http://newsapi.org/v2/top-headlines?' + 'country=nz&' + 'apiKey=0ec38d938d324e70a359a09a2ff04048'
+			url: 'http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=0ec38d938d324e70a359a09a2ff04048'
 		})
 		.then(function (response){
+			console.log(response.data.totalResults)
+			if (response.data.totalResults > 20) {
+				booleans.totalResults = 20
+			} else {
+				booleans.totalResults = response.data.totalResults
+			}
 			app.newsData = response;
+			console.log(app.newsData)
 		});
 	}
 
-	axiosCall();
+
+
+	// Vue.component('component-article', {
+	// 	template: `
+	// 	<div class="newsBlock" v-on:click="articleFullView(0)">
+	// 		<img v-bind:src="newsData.data.articles[0].urlToImage">
+	// 		<div class="textCont">
+	// 			<h2>HELLO {{newsData.data.articles[0].title}}</h2>
+	// 			<h3>{{newsData.data.articles[0].author}} <span class="date">{{newsData.data.articles[0].publishedAt}}</span></h3>
+	// 			<p class="article">{{newsData.data.articles[0].description}}</p>
+	// 		</div>
+	// 	</div>
+	// 	`
+	// })
 
 })(); // iffe ENDS
